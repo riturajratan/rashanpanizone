010e2fbd800d27b103851beffebc659c3a3a5ab5
Rituraj <riturajratan@gmail.com>
2024-12-12 16:54:56 +0530

    Update Hello.tsx

--- web/app/components/Hello.tsx
+++ web/app/components/Hello.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 export default function Hello() {
-  const [text] = useState("Hello world from client $1000");
+  const [text] = useState("Hello world from client $10000000");
 
   return <div className="text-lg font-bold">{text}</div>;
 }
